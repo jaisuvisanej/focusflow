24621b6b8fdeb1d6fd9b3d48eddb35b930d66e1a
jaisuvisane <jaisuvisanej@gmail.com>
2026-03-31 17:16:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('/*', cors(corsOptions)); // 🔥 IMPORTANT FIX
 
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true }));
